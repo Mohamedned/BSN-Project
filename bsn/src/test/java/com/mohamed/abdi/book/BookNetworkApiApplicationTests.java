ecb6ebcc153937c417c0b90b7adcc93eea79c6ec
Mohamed Abdi <Mohamed.Abdi@prorail.nl>
2024-05-10 07:26:07 +0200

    -

---
+++ bsn/src/test/java/com/mohamed/abdi/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.abdi.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
