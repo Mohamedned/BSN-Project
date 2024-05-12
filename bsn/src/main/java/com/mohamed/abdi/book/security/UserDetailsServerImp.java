fd618fad564a62895ddcdbf141317f07506cd497
Mohamed Abdi <Mohamed.Abdi@prorail.nl>
2024-05-12 16:06:05 +0200

    Implement JWT

---
+++ bsn/src/main/java/com/mohamed/abdi/book/security/UserDetailsServerImp.java
@@ -0,0 +1,22 @@
+package com.mohamed.abdi.book.security;
+
+import com.mohamed.abdi.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServerImp implements UserDetailsService {
+
+    private final UserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
